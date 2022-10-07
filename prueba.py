3d93ed355e02bf97ba981c48a000005d92021785
AmetJim123 <ametjimenezjg@unimagdalena.edu.co>
2022-10-06 22:01:22 -0500

    update

--- prueba.py
+++ prueba.py
@@ -1,18 +1,22 @@
-from flask import (Flask, redirect, url_for)
+from flask import (Flask,
+redirect,
+url_for,
+render_template
+)
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello, this is my first proyect in Flask</h1>"
+    return render_template("index.html")
 
-@app.route("/user/<name>/")
-def user(name):
-    return f"Hey, long time no see, eh {name}.."
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    return render_template()
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
     app.run()
